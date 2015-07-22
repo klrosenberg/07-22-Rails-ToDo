5f823228be46079a20725616aa18fdcd576aaed4
Kerry Rosenberg <rosenberg.kerry@gmail.com>
2015-07-22 16:05:21 -0500

    Add route name to get user by id

--- config/routes.rb
+++ config/routes.rb
@@ -10,9 +10,9 @@ Rails.application.routes.draw do
   
   post "users" => 'users#create'
   
-  get "users/new" => 'users#new' :as => "new_post"
+  get "users/new" => 'users#new', :as => "new_user"
   
-  get "users/:id" => 'users#show'
+  get "users/:id" => 'users#show', :as => "user"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
