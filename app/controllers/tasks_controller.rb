102e1bac228e4c4950511df0bd7e07b9d8c6f8d0
Kerry Rosenberg <rosenberg.kerry@gmail.com>
2015-07-22 16:30:41 -0500

    Task controller with CRUD

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,23 +1,51 @@
 class TasksController < ApplicationController
-  
+
   def index
-    @user = User.find(id)
-    if @tasks = Tasks.where("user.id = @user.id")
-    else
-    end
+    @tasks = Task.all
   end
   
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path
+  end
+  
+  def show
+    @task = Task.find(params[:id])
+    # Loads view in views/tasks/show
+  end
+  
+  # Form
+  def edit
+    @task = Task.find(params[:id])
+  end
+  
+  def update
+    @task = Task.find(params[:id])
+    if @task.update_attributes(task_params)
+      redirect_to tasks_path
+    else
+      render "edit"
+    end
+  end
+
   # Form
   def new
     @task = Task.new
   end
   
   def create
-    @task = Task.new(user_params)
+    @task = Task.new(task_params)
     if @task.save
       redirect_to tasks_path
     else
       render "new"
     end
+  end
+  
+  private
+  
+  def task_params
+    params[:task].permit(:description)
   end
 end
